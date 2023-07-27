a2f0162fd649aa160b76783f5ebeb6e63ea3a7c2
mhmahdi97 <mhmahdi97@gmail.com>
2023-07-27 19:42:28 +0600

    problem 5 solved

--- index.js
+++ index.js
@@ -11,3 +11,41 @@ function reverseString(inputString) {
 }
 
 
+// Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Task 5: Implement a simple JavaScript calculator. The calculator should take two numbers and an operator (+, -, *, /) as input and return the result of the operation.
+
+function simpleCalculator(num1, num2, operator) {
+    if (operator === '+') return num1 + num2
+    if (operator === '-') return num1 - num2
+    if (operator === '*') return num1 * num2
+    if (operator === '/') return num1 / num2
+
+    else return 'error'
+}
+
+
+
+
+
+
+
+
+// console.log(reverseString("Hi, darling"))
+// console.log(simpleCalculator(4, 5, '-'))
