48e3eef8b946fca0070c85fa1762332eda621461
mhmahdi97 <mhmahdi97@gmail.com>
2023-07-27 19:51:03 +0600

    solved problem 7

--- index.js
+++ index.js
@@ -47,5 +47,44 @@ function simpleCalculator(num1, num2, operator) {
 
 
 
+// Task 7: Implement a function that converts a Roman numeral to an integer. The function should take a Roman numeral string (e.g., "IX" or "XXI") as input and return the corresponding integer value.
+
+function romanToInteger(roman) {
+  const romanNumerals = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+  };
+
+  let result = 0;
+
+  for (let i = 0; i < roman.length; i++) {
+    const currentSymbolValue = romanNumerals[roman[i]];
+    const nextSymbolValue = romanNumerals[roman[i + 1]];
+
+    if (nextSymbolValue > currentSymbolValue) {
+      result += nextSymbolValue - currentSymbolValue;
+      i++; 
+    } else {
+      result += currentSymbolValue;
+    }
+  }
+
+  return result;
+}
+
+
+
+
+
+
+
+
+
 // console.log(reverseString("Hi, darling"))
 // console.log(simpleCalculator(4, 5, '-'))
+// console.log(romanToInteger('V'))
